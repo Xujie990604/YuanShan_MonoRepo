48aa0a51f9b0a52118e12f770ce564f7d849ab53
徐杰 <56837969+Xujie990604@users.noreply.github.com>
2025-07-08 14:47:47 +0800

    添加 dayjs 和 axios 作为外部依赖，并在 Vite 配置中进行相应设置；同时删除 HelloWorld 组件以简化项目结构。

--- apps/car/src/App.vue
+++ apps/car/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+// 使用全局变量
 import dayjs from 'dayjs'
 console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'))
 
 
+// 使用全局变量
 import axios from 'axios'
-axios.get('http://jsonplaceholder.typicode.com/posts').then(res => {
+axios.get('http://jsonplaceholder.typicode.com/posts').then((res: any) => {
   console.log(res)
 })
 </script>
--- apps/car/vite.config.ts
+++ apps/car/vite.config.ts
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+       // 确保外部化处理那些你不想打包进库的依赖
+      external: ['axios', 'dayjs'],
+      // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+      output: {
+        format: 'umd',
+        globals: {
+          axios: 'axios',
+          dayjs: 'dayjs',
+        },
+      },
+    },
+  }
 })
